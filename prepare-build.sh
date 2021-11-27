91d62e1661a6b64c7dedb8edb725fbd5f0c5f311
binaryeater <>
2021-11-27 05:58:05 +0000

    Minor fix to prepare-build.sh

--- prepare-build.sh
+++ prepare-build.sh
@@ -13,9 +13,8 @@ Il2CppInspector -i PTD.apk --select-outputs --cpp-out ./out/cpp
 rm ./out/cpp/appdata/il2cpp-functions.h
 cp ./out/cpp/appdata/*.h ./hook/appdata/
 
+apktool d -f ./PTD.apk -o ./out/apktool
 rm -rf ./out/main
 mkdir -p ./out/main
-unzip -j PTD.apk lib/armeabi-v7a/libmain.so -d ./out/main/
+cp ./out/apktool/lib/armeabi-v7a/libmain.so ./out/main/
 patchelf ./out/main/libmain.so --add-needed libhook.so
-
-apktool d -f ./PTD.apk -o ./out/apktool
