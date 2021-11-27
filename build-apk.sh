c49b31137d3fdb15a4dcf6c35c5d48b9ef107bba
binaryeater <>
2021-11-27 06:36:12 +0000

    Use apksigner for signing the apk

--- build-apk.sh
+++ build-apk.sh
@@ -9,7 +9,7 @@ pushd ./out/apktool/
 apktool b -f -o ../PTD_tmp.apk
 popd
 
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore "$KEYSTORE_FILE" ./out/PTD_tmp.apk alias_name
 rm -f ./out/PTD_modded.apk
-zipalign -v 4 ./out/PTD_tmp.apk  ./out/PTD_modded.apk
+zipalign -v 4 ./out/PTD_tmp.apk ./out/PTD_modded.apk
 rm ./out/PTD_tmp.apk
+apksigner sign --ks "$KEYSTORE_FILE" ./out/PTD_modded.apk
