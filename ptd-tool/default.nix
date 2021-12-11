55ec4c26cbd18b5f051a11a3cce598e670059a80
binaryeater <>
2021-12-11 05:50:28 +0000

    Update ptd-tool

--- ptd-tool/default.nix
+++ ptd-tool/default.nix
@@ -6,12 +6,12 @@
 }:
 buildGoApplication rec {
   pname = "ptd-tool";
-  version = "8fbf613b1f9d3c76fcce052daf3c4f00ddd585a4";
+  version = "19c9e555b92b4453fa7dbff16a67fe9e0e4e06f8";
   buildInputs = [ jq ];
   nativeBuildInputs = [ unzip ];
   src = fetchurl {
     url = "https://gitee.com/binaryeater/ptd-tool/repository/archive/${version}.zip";
-    sha256 = "1dfsslw2ngbxnn5kf1bwi01d91gl3b06jqibj01i6sban794sjdi";
+    sha256 = "1x1pp7nikf8d9l5a1i3j9mg5rs6nf3xfcsaxi8in6vv01kfq5knr";
   };
   modules = ./gomod2nix.toml;
   postBuild = ''
