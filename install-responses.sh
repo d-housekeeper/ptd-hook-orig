410c42397365b9463017a35d153c305fbe712147
binaryeater <>
2021-12-23 03:33:38 +0000

    Push responses to /data/local/tmp first to avoid issues on Android11+

--- install-responses.sh
+++ install-responses.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 export MSYS_NO_PATHCONV=1 # required on msys
-adb push ./out/Responses /storage/emulated/0/Android/data/com.square_enix.android_googleplay.PTD/files/
+adb push ./out/Responses /data/local/tmp/
+adb shell mv /data/local/tmp/Responses /storage/emulated/0/Android/data/com.square_enix.android_googleplay.PTD/files/
